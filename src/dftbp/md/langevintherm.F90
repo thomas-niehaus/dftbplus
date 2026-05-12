5e12140eeeee2c0b1e409ac79d63f8d87909c18d
Thomas Niehaus <thomas.niehaus@univ-lyon1.fr>
2026-05-12 10:17:11 +0200

    First working

--- src/dftbp/md/langevintherm.F90
+++ src/dftbp/md/langevintherm.F90
@@ -21,8 +21,7 @@ module dftbp_md_langevintherm
   private
   public :: TLangevinThermInput
   public :: TLangevinTherm, TLangevinTherm_init
-
-
+  
   !> Thermostat specific input data for the Langevin thermostat
   type :: TLangevinThermInput
 
@@ -56,6 +55,11 @@ module dftbp_md_langevintherm
 
     !> MD timestep
     real(dp) :: deltaT
+
+    ! Regional data storage
+    logical :: tRegioTherm
+    integer :: startReg(2), endReg(2)
+    real(dp) :: regKT(2), energyExchange(2)
 
   contains
 
@@ -92,6 +96,11 @@ contains
     !> MD time step
     real(dp), intent(in) :: deltaT
 
+    integer :: unit, iStat, iReg, iAt
+    real(dp) :: tempK
+    ! Boltzmann constant in atomic units: approx 3.166811e-6 Hartree/K
+    real(dp), parameter :: KB_AU = 3.16681153d-6
+
     call move_alloc(pRanlux, this%pRanlux)
     this%nAtom = size(masses)
     this%mass = masses
@@ -99,7 +108,33 @@ contains
     this%gamma = input%gamma
     this%pMDFramework = pMDFramework
     this%deltaT = deltaT
-
+    
+    ! Read regional information once during initialization
+    open(newunit=unit, file='thermoRange.dat', status='old', iostat=iStat) 
+    if (iStat /= 0) then
+      this%tRegioTherm = .false.
+    else
+      this%tRegioTherm = .true.
+      this%energyExchange = 0.0_dp
+      
+      do iReg = 1, 2
+        read(unit, *, iostat=iStat) this%regKT(iReg), this%startReg(iReg), this%endReg(iReg)
+        if (iStat /= 0) call error("Error: Could not read line for region in thermoRange.dat")
+        if (this%startReg(iReg) < 1 .or. this%endReg(iReg) > this%nAtom) then
+          call error("Thermostat region indices out of physical bounds (1 to nAtoms).")
+        end if
+        if (this%startReg(iReg) > this%endReg(iReg)) then
+          call error("startReg > endReg in thermoRange.dat.")
+        end if
+        ! Convert K to atomic units (Hartree)
+        this%regKT(iReg) = this%regKT(iReg) * KB_AU
+      end do
+      if (.not. (this%endReg(1) < this%startReg(2) .or. this%endReg(2) < this%startReg(1))) then
+        call error("Thermostat regions 1 and 2 overlap in thermoRange.dat.")
+      end if
+    end if
+    close(unit)
+    
   end subroutine TLangevinTherm_init
 
 
@@ -113,7 +148,7 @@ contains
     real(dp), intent(out) :: velocities(:,:)
 
     real(dp) :: kT
-    integer :: ii
+    integer :: ii, iReg
 
     @:ASSERT(all(shape(velocities) <= [3, this%nAtom]))
 
@@ -121,6 +156,11 @@ contains
     if (kT < minTemp) then
       call error("Langevin thermostat not supported at zero temperature")
     end if
+
+    if(this%tRegioTherm) then
+      kT = sum(this%regKT)/2.0_dp
+    end if 
+    
     do ii = 1, this%nAtom
       call MaxwellBoltzmann(velocities(:,ii), this%mass(ii), kT, this%pRanlux)
     end do
@@ -141,7 +181,8 @@ contains
 
     real(dp), allocatable :: uniform(:), gaussian(:)
     real(dp) :: kT, xi
-    integer :: ii, nDeg
+    integer :: ii, nDeg, iReg
+    real(dp) :: v_sq_old, v_sq_new
 
     @:ASSERT(all(shape(velocities) <= [3, this%nAtom]))
     nDeg = 3 * this%nAtom
@@ -153,13 +194,31 @@ contains
     do ii = 1, nDeg, 2
       call boxMueller(gaussian(ii), gaussian(ii+1), uniform(ii), uniform(ii+1))
     end do
-    velocities(:, :) = velocities - this%gamma * this%deltaT * velocities
-    do ii = 1, this%nAtom
-      xi = sqrt(2.0_dp * this%mass(ii) * this%gamma * kT * this%deltaT) / this%mass(ii)
-      velocities(:, ii) = velocities(:, ii) + xi * gaussian((ii-1)*3+1:ii*3)
-    end do
-    call restFrame(this%pMDFramework, velocities, this%mass)
+    
+    if(this%tRegioTherm) then
+      do iReg = 1, 2
+        do ii = this%startReg(iReg), this%endReg(iReg)
+          v_sq_old = sum(velocities(:, ii)**2)
+          
+          velocities(:, ii) = velocities(:, ii) * (1.0_dp - this%gamma * this%deltaT)
+          xi = sqrt(2.0_dp * this%mass(ii) * this%gamma * this%regKT(iReg) * this%deltaT) / this%mass(ii)
+          velocities(:, ii) = velocities(:, ii) + xi * gaussian((ii-1)*3+1:ii*3)
+          
+          v_sq_new = sum(velocities(:, ii)**2)
+          this%energyExchange(iReg) = this%energyExchange(iReg) + &
+              0.5_dp * this%mass(ii) * (v_sq_new - v_sq_old)
+        end do
+      end do
+    else
+      velocities(:, :) = velocities * (1.0_dp - this%gamma * this%deltaT)
+      do ii = 1, this%nAtom
+        xi = sqrt(2.0_dp * this%mass(ii) * this%gamma * kT * this%deltaT) / this%mass(ii)
+        velocities(:, ii) = velocities(:, ii) + xi * gaussian((ii-1)*3+1:ii*3)
+      end do
+    end if 
 
+    call restFrame(this%pMDFramework, velocities, this%mass)
+ 
   end subroutine TLangevinTherm_updateVelocities
 
 
@@ -172,7 +231,7 @@ contains
     !> File unit to write thermostat state out to
     integer, intent(in) :: fd
 
-    ! no internal state, nothing to do
+    write(fd,"(a,1x,3e20.10,2x,3e20.10)") 'Acc. energy loss in reservoirs:', this%energyExchange
 
   end subroutine TLangevinTherm_writeState
 
